9db2a01c868a347bc792c8f559b9f88837962042
marathonman4202 <roy.haggerty@oregonstate.edu>
2014-11-11 11:34:11 -0800

    Added a few helper scripts

---
+++ constants.py
@@ -0,0 +1,19 @@
+# constants.py
+"""
+File containing constants to be used in various WW2100 graphics scripts.
+"""
+import datetime
+
+day_of_year_apr1 = 91
+day_of_year_oct1 = 274
+day_of_year_aug15 = 227
+day_of_year_jan1 = 1
+Jan1_1900 = datetime.date(1900,1,1)
+Jan1_2010 = datetime.date(2010,1,1)
+cfs_to_m3 = 0.0283168   #Cubic feet per second to cubic meters per second
+F_to_C = 1.0/1.8        #Fahrenheit to Celcius
+in_to_mm = 25.4         #Inches to milimeters
+acft_to_m3 = 1233.48
+acftperday_to_m3s = acft_to_m3/86400.
+seconds_in_yr = 86400.*365.25
+days_in_yr = 365
