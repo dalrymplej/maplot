5068abfcbb6de3cc176ec167c0bf1906d684543d
marathonman4202 <roy.haggerty@oregonstate.edu>
2014-11-16 14:28:43 -0800

    sub-basin area and plotting shapefiles

--- Ex1.py
+++ Ex1.py
@@ -22,19 +22,22 @@ im = plt.imread('C:\\code\\maplot\\ElevationMap_Willamette.png')
 WBmap.imshow(im, origin='upper') #interpolation='lanczos', 
 
 subbasin_pour_points= {\
-            'McKenzie':(-123.1043,44.1256,1,3307033881.96),\
-            'Middle Fork Willamette':(-122.9073,43.9998,2,3482874058.62),\
-            'Upper Yamhill':(-123.1445,45.2257,3,1340602668.23),\
-            'Pudding':(-122.7162,45.2976,4,2268590002.85),\
-            'Clackamas':(-122.6077,45.3719,5,2434914144.62),\
-            'Long Tom':(-123.2569,44.3807,6,1050268949.3),\
-            'Marys':(-123.2615,44.5564,7,778831948.728),\
-            'North Santiam':(-123.1432,44.7501,8,1976850713.48+2694079717.91),\
-            'South Santiam':(-123.007,44.6855,9,2694079717.91),\
-            'Tualatin':(-122.6501,45.3377,10,1829685666.99),\
-            'Coast Fork Willamette':(-123.0082,44.0208,11,1691632167.43),\
-            'Willamette':(-122.7651,45.6537,12, 29728000000.)\
+            'McKenzie':                 (-123.1043, 44.1256,    1,  3307033881.96),\
+            'Middle Fork Willamette':   (-122.9073, 43.9998,    2,  3482874058.62),\
+            'Upper Yamhill':            (-123.1445, 45.2257,    3,  1340602668.23),\
+            'Pudding':                  (-122.7162, 45.2976,    4,  2268590002.85),\
+            'Clackamas':                (-122.6077, 45.3719,    5,  2434914144.62),\
+            'Long Tom':                 (-123.2569, 44.3807,    6,  1050268949.3),\
+            'Marys':                    (-123.2615, 44.5564,    7,  778831948.728),\
+            'North Santiam':            (-123.1432, 44.7501,    8,  1976850713.48+2694079717.91),\
+            'South Santiam':            (-123.007,  44.6855,    9,  2694079717.91),\
+            'Tualatin':                 (-122.6501, 45.3377,    10, 1829685666.99),\
+            'Coast Fork Willamette':    (-123.0082, 44.0208,    11, 1691632167.43),\
+            'Willamette':               (-122.7651, 45.6537,    12 , 29728000000.)\
             }
+
+shp = 'C:\\Users\\haggertr\\Desktop\\Documents\\work - OSU\\research\\WW2100\\Research\\shapefiles\\Sub_Area_gc2'
+WBmap.readshapefile(shp, 'metadata', drawbounds=True,linewidth=0.25, color='k', )
 
 subbasin_pour_points_coords = [subbasin_pour_points[key] for key in subbasin_pour_points]
 subbasin_pour_points_lons = [subbasin_pour_points_coords[i][0] for i in range(len(subbasin_pour_points_coords))]
@@ -55,4 +58,4 @@ cmap1 = mpl.colors.LinearSegmentedColormap.from_list('my_cmap',['white','blue'],
 WBmap.scatter(x, y, marker='o',  s=100, lw=0,c=colord,cmap = cmap1)
 #plt.show()
 file_graphics = 'WB.png'
-plt.savefig(file_graphics, format="png", dpi=300)
+plt.savefig(file_graphics, format="png", dpi=600)
