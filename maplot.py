8d0fa14419681ae4b759d7d72094d90ec13bc914
Roy Haggerty <roy.haggerty@oregonstate.edu>
2015-05-13 07:25:16 -0700

    error in scatter plot color dimensions
    For scatter plot, dimensions of x, y were 13 because of legend used in
    line plots.  For scatter plots, need x, y dimension of 12.  Deleted last
    element of x, y.

--- maplot.py
+++ maplot.py
@@ -553,7 +553,7 @@ if subbasins_loop:
 #    plots_to_plot = range(4)
 #    plots_to_plot.extend([9])
 #    plots_to_plot.extend([4,45,5,8,9])
-    plots_to_plot.extend([45])
+    plots_to_plot.extend([0])
     
 if reservoirs_loop:
 #    plots_to_plot.extend([101,102,103])
@@ -607,7 +607,7 @@ for plot_num in plots_to_plot:
         
         colord = np.array(data1_spQ)
         
-        x,y=WBmap(subbasin_data_lons,subbasin_data_lats)
+        x,y=WBmap(subbasin_data_lons[:12],subbasin_data_lats[:12])
         cmap1 = mpl.colors.LinearSegmentedColormap.from_list('my_cmap',['white','blue'],128)
         WBmap.scatter(x, y, marker='o',  s=data1_size, lw=0,c=colord,cmap = cmap1)
         
@@ -660,7 +660,7 @@ for plot_num in plots_to_plot:
        
         colord = np.array(diff_drought_days)
         
-        x,y=WBmap(subbasin_data_lons,subbasin_data_lats)
+        x,y=WBmap(subbasin_data_lons[:12],subbasin_data_lats[:12])
         cmap1 = mpl.colors.LinearSegmentedColormap.from_list('my_cmap',['blue','white','red'],128)
         WBmap.scatter(x, y, marker='o',  s=data1_size, lw=0,c=colord,cmap = cmap1)
         
@@ -703,7 +703,7 @@ for plot_num in plots_to_plot:
        
         colord = np.array(diff_ann_precip)
         
-        x,y=WBmap(subbasin_data_lons,subbasin_data_lats)
+        x,y=WBmap(subbasin_data_lons[:12],subbasin_data_lats[:12])
         cmap1 = mpl.colors.LinearSegmentedColormap.from_list('my_cmap',['blue','white'],128)
         WBmap.scatter(x, y, marker='o',  s=data1_size, lw=0,c=colord,cmap = cmap1)
         
@@ -746,7 +746,7 @@ for plot_num in plots_to_plot:
        
         colord = np.array(diff_winter_temp)
         
-        x,y=WBmap(subbasin_data_lons,subbasin_data_lats)
+        x,y=WBmap(subbasin_data_lons[:12],subbasin_data_lats[:12])
         cmap1 = mpl.colors.LinearSegmentedColormap.from_list('my_cmap',['white','red'],128)
         WBmap.scatter(x, y, marker='o',  s=200., lw=0,c=colord,cmap = cmap1)
         
